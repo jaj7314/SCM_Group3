b0412127ec5025a93ca1186fe1fee77c9d0c7e56
zzzykf <zzzykf@gmail.com>
2017-11-23 14:34:56 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 	include('config.php');
 	session_start();
-	$conn = pg_connect("host='$HOST' port='$PORT' dbname='$DATABASE_NAME' user='$DATABASE_USER' password='$DATABASE_PASSWORD'")
+	$conn = pg_connect("host='$HOST' port='$PORT' dbname='$DATABASE_NAME' user='$DATABASE_USER' password='$DATABASE_PASSWORD'");
 		
 ?>
