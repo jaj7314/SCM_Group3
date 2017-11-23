2b7b8f473f6ded980522c3c8cb53f1e4a3ebfc59
zzzykf <zzzykf@gmail.com>
2017-11-23 14:31:40 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
   $HOST = 'ec2-54-221-207-143.compute-1.amazonaws.com';
-  $port = '5432';
+  $PORT = '5432';
   $DATABASE_NAME = 'd1583ojbjir9jc';
   $DATABASE_USER = 'olrmqdczslmujh';
   $DATABASE_PASSWORD = '63f0cb1a16cf6b40b5b1a71e3fb7f61f0bbf8484ef36c73a3c9d668ef4d76b66';
