187dddb1d5a46443168536df8a93f628a565f90d
zzzykf <zzzykf@gmail.com>
2017-11-23 14:44:04 +0800

    Update db.php

--- config.php
+++ config.php
@@ -4,5 +4,4 @@
   $DATABASE_NAME = 'd1583ojbjir9jc';
   $DATABASE_USER = 'olrmqdczslmujh';
   $DATABASE_PASSWORD = '63f0cb1a16cf6b40b5b1a71e3fb7f61f0bbf8484ef36c73a3c9d668ef4d76b66';
-	echo $HOST;
 ?>
--- db.php
+++ db.php
@@ -2,5 +2,5 @@
 	include('config.php');
 	session_start();
 	$conn = pg_connect("host='$HOST' port='$PORT' dbname='$DATABASE_NAME' user='$DATABASE_USER' password='$DATABASE_PASSWORD'");
-		
+	echo "host='$HOST' port='$PORT' dbname='$DATABASE_NAME' user='$DATABASE_USER' password='$DATABASE_PASSWORD'";
 ?>
