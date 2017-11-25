1439f880f456e6d18d4df744c44716b3d953d2aa
Cik Eiyfa <33578235+CikEiyfa@users.noreply.github.com>
2017-11-25 20:25:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
                     <div class="menu-row">
                         <div class="menus" id="menu">
                             <div onclick="location.href='pasta.php'">
-                                MENU
+                                TESt
                             </div>
                             <ul>
                                 <li onclick="location.href='pasta.php'">Pasta</li>
